518e1c825f0cb002b873a908d19ca685e02f652f
Jeff Hunken <jhunken@gmail.com>
2013-06-16 18:14:55 -0700

    scrapping sql server CE
    too much trouble deploying to app harbor. Using flat csv file instead

--- stocktikr.API/Controllers/SymbolController.cs
+++ stocktikr.API/Controllers/SymbolController.cs
@@ -7,24 +7,23 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web;
 using System.Text;
-using stocktikr.API.DAL;
+
+using System.IO;
+using stocktikr.API.Models;
 
 namespace stocktikr.API.Controllers
 {
     public class SymbolController : ApiController
     {
-        private StockDataContext db = new StockDataContext();
 
+        private Stock _stock = new Stock();
         // GET api/symbol
-        public IEnumerable<NYSE> Get()
+        public IEnumerable<Stock> Get()
         {
 
-            var stocks = from s in db.NYSEs
-                         select s;
 
-            return stocks.ToList();
-
-
+            return _stock.GetStocks();
+            
         }
 
         // GET api/symbol?term=
@@ -32,7 +31,7 @@ namespace stocktikr.API.Controllers
         public IEnumerable<AutoCompleteModel> Get(string term)
         {
 
-            var stocks = from s in db.NYSEs
+            var stocks = from s in _stock.GetStocks()
                          where s.Symbol.Contains(term) || s.Description.Contains(term)
                          select new AutoCompleteModel
                          {
@@ -49,12 +48,12 @@ namespace stocktikr.API.Controllers
         public JsonResult GetBySymbol(string symbol)
         {
             //get stock from database, then add random price
-            var stock = (from s in db.NYSEs
+            var stock = (from s in _stock.GetStocks()
                          where s.Symbol.ToLower() == symbol.ToLower()
                          select s).FirstOrDefault();
             if (stock != null)
             {
-
+                stock.Price = stock.GetRandomPrice(stock);
                 return new JsonResult { Data = new { success = true, stock = stock }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
             else
@@ -65,20 +64,21 @@ namespace stocktikr.API.Controllers
         }
 
         
-        public JsonResult PostBulkUpdate(List<NYSE> stocks)
+        public JsonResult PostBulkUpdate(List<Stock> stocks)
         {
             //receive list of symbols for bulk price update
             if (stocks.Count() > 0)
             {
-                List<NYSE> updatedStocks = new List<NYSE>();
+                List<Stock> updatedStocks = new List<Stock>();
                 foreach (var stock in stocks)
                 {
-                    var dbStock = (from s in db.NYSEs
+                    var dbStock = (from s in _stock.GetStocks()
                                    where s.Symbol.ToLower() == stock.Symbol.ToLower()
                                  select s).FirstOrDefault();
                     if (dbStock != null)
                     {
                         //dbStock.LastPrice = stock.Price;
+                        dbStock.Price = dbStock.GetRandomPrice(stock);
                         updatedStocks.Add(dbStock);
                     }
 
---
+++ stocktikr.API/Models/Stock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace stocktikr.API.Models
+{
+    public class Stock
+    {
+        string path = HttpContext.Current.Server.MapPath("~/resources/symbolNames.txt");
+        public IEnumerable<Stock> GetStocks()
+        {
+
+            string[] allLines = File.ReadAllLines(path);
+            IEnumerable<Stock> stocks = from s in allLines
+                                        let data = s.Split('\t')
+                                        select new Stock
+                                        {
+                                            Symbol = data[0],
+                                            Description = data[1]
+                                        };
+            return stocks;
+
+
+        }
+        public decimal GetRandomPrice(Stock stock)
+        {
+
+            //get random "price" based on ascii values of stock symbol
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(stock.Symbol);
+            decimal price = 0;
+            for (int i = 0; i < asciiBytes.Length; i++)
+            {
+                price += asciiBytes[i];
+            }
+
+            //we have base price. Let's add random variance
+            Random random = new Random();
+            decimal percentChange = (decimal)(random.NextDouble()) / 100;
+            price = (price * percentChange) + price;
+            price = Math.Round(price, 2);
+            return price;
+
+
+        }
+        public decimal Price { get; set; }
+        public string Symbol { get; set; }
+        public string Description { get; set; }
+
+        public decimal LastPrice { get; set; }
+
+
+
+    }
+}
